ac3d10150ef52fbf434b1311985aca3bd886395d
kagzter <kagzter@549372f6-1889-11de-8120-8d224146dbbc>
2009-03-25 18:27:00 +0000

    Diskio modified to include SD card read/write.

--- src/diskio.c
+++ src/diskio.c
@@ -16,6 +16,13 @@
 #define MMC		1
 #define USB		2
 
+/*************************************************************************/
+/*prototypes*/
+char SPI(char d);
+char Command(char, uint16_t, uint16_t, char );
+int MMC_disk_initialize(void);
+int MMC_disk_write(char*, long, char);
+int MMC_disk_read(char*, long, char);
 
 
 /*-----------------------------------------------------------------------*/
@@ -202,3 +209,94 @@ DRESULT disk_ioctl (
 	return RES_PARERR;
 }
 
+
+
+
+char SPI(char d) {  // send character over SPI
+	char received = 0;
+	SPDR = d;
+	while(!(SPSR & (1<<SPIF)));
+	received = SPDR;
+	return (received);
+}
+
+
+char Command(char befF, uint16_t AdrH, uint16_t AdrL, char befH )
+{	// sends a command to the MMC
+	SPI(0xFF);
+	SPI(befF);
+	SPI((uint8_t)(AdrH >> 8));
+	SPI((uint8_t)AdrH);
+	SPI((uint8_t)(AdrL >> 8));
+	SPI((uint8_t)AdrL);
+	SPI(befH);
+	SPI(0xFF);
+	return SPI(0xFF);	// return the last received character
+}
+
+int MMC_disk_initialize(void){
+	char i;
+	PORTB |= (1 << SPICS); // disable MMC
+	// start MMC in SPI mode
+	for(i=0; i < 10; i++) SPI(0xFF); // send 10*8=80 clock pulses
+	PORTB &= ~(1 << SPICS); // enable MMC
+
+	if (Command(0x40,0,0,0x95) != 1) goto mmcerror; // reset MMC
+
+st: // if there is no MMC, prg. loops here
+	if (Command(0x41,0,0,0xFF) !=0) goto st;
+	return 1;
+mmcerror:
+	fprintf(stdout,"MMC init error");
+	return 0;
+}
+
+int MMC_disk_write(char *buff, long sector, char count){
+	int i;
+	uint8_t c;
+	// 512 byte-write-mode
+	if (Command(0x58,((sector>>16)&0xff),(sector & 0xff),0xFF) !=0) {
+		fprintf(stdout,"MMC: write error 1 ");
+		return 1;	
+	}
+	SPI(0xFF);
+	SPI(0xFF);
+	SPI(0xFE);
+	// write ram sectors to MMC
+	for (i=0;i<count;i++) {
+		SPI(buff[i]);
+	}
+	// at the end, send 2 dummy bytes
+	SPI(0xFF);
+	SPI(0xFF);
+
+	c = SPI(0xFF);
+	c &= 0x1F; 	// 0x1F = 0b.0001.1111;
+	if (c != 0x05) { // 0x05 = 0b.0000.0101
+		fprintf(stdout,"MMC: write error 2 ");
+		return 1;
+	}
+	// wait until MMC is not busy anymore
+	while(SPI(0xFF) != (char)0xFF);
+	return 0;
+}
+
+int MMC_disk_read(char *buff, long sector, char count){
+	int i;
+	// 512 byte-read-mode 
+	if (Command(0x51,((sector>>16)&0xff),(sector & 0xff),0xFF) != 0) {
+		fprintf(stdout,"MMC: read error 1 ");
+		return 1;
+	}
+	// wait for 0xFE - start of any transmission
+	// ATT: typecast (char)0xFE is a must!
+	while(SPI(0xFF) != (char)0xFE);
+
+	for(i=0; i < 512; i++) {
+		buff[i] = SPI(0xFF);
+	}
+	// at the end, send 2 dummy bytes
+	SPI(0xFF); // actually this returns the CRC/checksum byte
+	SPI(0xFF);
+	return 0;
+}
