f70dc5b7be67a5b3c7c5b34e98febb1aee61f513
kagzter <kagzter@549372f6-1889-11de-8120-8d224146dbbc>
2009-04-24 06:13:59 +0000

    512 byte ring buffer added to aid flow control. Data is now sent to DAC at 8KHz(we're using .wav files at 8KHz sampling frequency) via a timer interrupt. Transmitting device needs to be adjusted for slightly higher transmit speed, just enough to cover the read period.

--- trunk/dac/dac.c
+++ trunk/dac/dac.c
@@ -24,10 +24,10 @@ void SPI_MasterInit(void);							//initialise the SPI port
 void init(void);												//microcon initialise
 
 // RXC ISR variables								
-volatile char r_index;  //current string index
-volatile char r_buffer[16];	//input string 
-volatile char r_ready;  //flag for receive done
-volatile char r_char;  //current character  
+volatile char r_buffer[512];	//ring buffer 
+volatile unsigned int rpos = 0;  //keeps track of write position
+volatile unsigned int wpos = 0;  //keeps track of read position
+
 
 uint8_t count = 0;
 int16_t time = 0;
@@ -37,33 +37,21 @@ uint16_t dac_value = 0;
 
 ISR (USART0_RX_vect) {
 
-/*	r_char = UDR0 ;    //get a char
-	UDR0 = r_char;    //then print it
-	//build the input string
-	if (r_char != '\r') 	// Is the input a <enter>?
-	begin 
-		if (r_char == '\b')	// Is the input a backspace? 
-		begin
-			putchar(' '); 	// erase the character on the screen
-			putchar('\b');	// backup
-			--r_index ;		// wipe a character from the string
-		end
-		else   
- 			r_buffer[r_index++] = r_char ; // add a character to the string
-	end
-	else 					// Human pressed <enter>
-	begin
-		putchar('\n');  			//use putchar to avoid overwrite
-		r_buffer[r_index] = 0x00;   //zero terminate
-		r_ready = 1;				//signal cmd processor
-		UCSR0B ^= (1<<RXCIE0) ;   	//stop rec ISR -- clear rxc
-	end
-*/
-	r_char = UDR0;
-	r_ready = 1;
-	UCSR0B &= ~(1<<RXCIE0) ;
+	r_buffer(wpos) = UDR0;
+	wpos = (wpos+1)&0x1ff;  //512 counter
 }
 
+//**********************************************************
+//timer 2 overflow ISR
+ISR (TIMER2_COMPA_vect) 
+begin      
+  //Decrement the time if not already zero
+	if(rpos != wpos) {
+		dac_value = (0x4000 | (r_buffer(rpos)));
+		SPI_MasterTransmit(dac_value);
+		rpos = (rpos+1)&0x1ff;  //512 counter
+	}
+end  
 
 
 ISR (TIMER0_COMPA_vect){
@@ -83,13 +71,6 @@ int main(void) {
 
 	//endless loop
 	while (1){
-		
-		if(r_ready == 1) {
-			dac_value = (0x4000 | (r_char));
-			SPI_MasterTransmit(dac_value);
-			r_ready = 0;
-			UCSR0B |= (1<<RXCIE0) ;
-		}
 
 		if (time == 0) {
 			PORTD = ~PORTD;
@@ -136,14 +117,22 @@ void SPI_MasterTransmit(unsigned int data){
 
 void init(void) {
 
-  //set up timer 0 for 1 mSec timebase 
-  TIMSK0= (1<<OCIE0A);	//turn on timer 0 cmp match ISR 
-  OCR0A = 249;  		//set the compare re to 250 time ticks
-  //set prescalar to divide by 64 
-  TCCR0B= 3; //0b00001011;	
-  // turn on clear-on-match
-  TCCR0A= (1<<WGM01) ;
-
+	//set up timer 0 for 1 mSec timebase 
+	TIMSK0= (1<<OCIE0A);	//turn on timer 0 cmp match ISR 
+	OCR0A = 249;  		//set the compare re to 250 time ticks
+	//set prescalar to divide by 64 
+	TCCR0B= 3; //0b00001011;	
+	// turn on clear-on-match
+	TCCR0A= (1<<WGM01) ;
+	
+	//set up timer 0 for 1 mSec timebase 
+	TIMSK2= (1<<OCIE2A);	//turn on timer 2 cmp match ISR 
+	OCR2A = 38;  		//set the compare re to 39 time ticks
+	//set prescalar to divide by 64 
+	TCCR2B= (1<<CS22); 
+	// turn on clear-on-match
+	TCCR2A= (1<<WGM21) ;
+	
 
 	DDRD = (1<<PIND2);
 	//init serial
