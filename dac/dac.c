d0cfc86601d87fb02494f7381cffac67ca185440
kagzter <kagzter@549372f6-1889-11de-8120-8d224146dbbc>
2009-04-25 04:59:28 +0000

    Currently requests updates in 512B blocks from tranmission station. Plays back 8bit, 8KHz music

--- dac/dac.c
+++ dac/dac.c
@@ -4,6 +4,8 @@
 #include <inttypes.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#include <math.h> 		// for sine
+
 
 #include "uart.h"
 
@@ -24,39 +26,43 @@ void SPI_MasterInit(void);							//initialise the SPI port
 void init(void);												//microcon initialise
 
 // RXC ISR variables								
-volatile char r_buffer[512];	//ring buffer 
+volatile char r_buffer[1026];	//ring buffer 
 volatile unsigned int rpos = 0;  //keeps track of write position
 volatile unsigned int wpos = 0;  //keeps track of read position
 
 
 uint8_t count = 0;
 int16_t time = 0;
-uint16_t dac_value = 0;
+volatile uint16_t dac_value = 0;
+uint16_t buff_size = 0;
 
 //*****************************************************************************
 
 ISR (USART0_RX_vect) {
 
-	r_buffer(wpos) = UDR0;
-	wpos = (wpos+1)&0x1ff;  //512 counter
+	r_buffer[wpos] = UDR0;
+	wpos = (wpos+1)&0x3ff;  //1024 counter
 }
 
 //**********************************************************
 //timer 2 overflow ISR
-ISR (TIMER2_COMPA_vect) 
-begin      
-  //Decrement the time if not already zero
+ISR (TIMER2_COMPA_vect) {
+      
+	//Decrement the time if not already zero
 	if(rpos != wpos) {
-		dac_value = (0x4000 | (r_buffer(rpos)));
+		dac_value = (0x4000 | (r_buffer[rpos]));
 		SPI_MasterTransmit(dac_value);
-		rpos = (rpos+1)&0x1ff;  //512 counter
+
+		rpos = (rpos+1)&0x3ff;  //1024 counter
+	
 	}
-end  
+}
 
 
 ISR (TIMER0_COMPA_vect){
 	//timer0 compare match
 	//count++;
+	
 	if(time>0) time--;
 }
 //*****************************************************************************
@@ -71,11 +77,21 @@ int main(void) {
 
 	//endless loop
 	while (1){
-
+		if (rpos <= wpos) {
+			buff_size = wpos - rpos;
+		} else if (rpos > wpos) {
+			buff_size = wpos + 1024 - rpos;
+		}
+		if (buff_size < 256) {
+			while (! (UCSR0A | (1<<UDRE0))){}
+  			UDR0 = 0x01;						//command to request more data
+		}
 		if (time == 0) {
 			PORTD = ~PORTD;
 			time = t1;
 		}
+		//SPI_MasterTransmit(dac_value);
+		//PORTC = ~PORTC;
 	}
 }
 
@@ -84,7 +100,7 @@ void SPI_MasterInit(void){
 	DDR_SPI = (1<<DD_MOSI)|(1<<DD_SCK)|(1<<SS);
 
 	// Enable SPI, Master, set clock rate fck/4
-	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0)|(1<<CPOL)|(0<<CPHA);
+	SPCR = (1<<SPE)|(1<<MSTR)|(1<<CPOL)|(0<<CPHA);
 
 	//Set double data rate for fck/2 operation
 	//SPSR = (1<<SPI2X);
@@ -116,7 +132,13 @@ void SPI_MasterTransmit(unsigned int data){
 }
 
 void init(void) {
-
+/*
+	for (int i=0; i<1024; i++)
+    {
+   		r_buffer[i] = ((char)(127.0 * sin(6.283*((float)i)/8))) + 127 ;
+    } 
+*/
+	
 	//set up timer 0 for 1 mSec timebase 
 	TIMSK0= (1<<OCIE0A);	//turn on timer 0 cmp match ISR 
 	OCR0A = 249;  		//set the compare re to 250 time ticks
@@ -135,6 +157,7 @@ void init(void) {
 	
 
 	DDRD = (1<<PIND2);
+	DDRC = (1<<PINC0);
 	//init serial
 	uart_init();
 	stdout = stdin = stderr = &uart_str;
